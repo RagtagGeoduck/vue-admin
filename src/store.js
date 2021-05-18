ab15c97a67649b19b62abdcb3ab5689721340c64
RagtagGeoduck <gjt675185496@gmail.com>
2021-05-18 23:37:04 +0800

    添加了nav收缩状态下探出框的样式

--- src/store.js
+++ src/store.js
@@ -1,11 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Cookie from 'cookie_js'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    isCollapse:false,
+    // isCollapse:false,
+    isCollapse:JSON.parse(sessionStorage.getItem("isCollapse")) || false,
+    // isCollapse:JSON.parse(localStorage.getItem("isCollapse")) || false,
+    // isCollapse: JSON.parse(Cookie.get("isCollapse")) || false,
     count: 10
   },
   getters:{
@@ -15,6 +19,9 @@ export default new Vuex.Store({
   mutations: {
     SET_COLLAPSE(state){
       state.isCollapse = !state.isCollapse;
+      sessionStorage.setItem('isCollapse', JSON.stringify(state.isCollapse));
+      // localStorage.setItem('isCollapse', JSON.stringify(state.isCollapse));
+      // Cookie.set("isCollapse2", JSON.stringify(state.isCollapse));
     },
     SET_COUNT(state, value){
       state.count += value
